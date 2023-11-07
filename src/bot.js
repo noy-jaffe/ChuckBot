d0f30ef1f4143de10dcb7394d7007ac21ed16fdf
Noy_Jaffe <noijaffe@gmail.com>
2023-11-07 11:18:41 +0200

    add constants

--- src/bot.js
+++ src/bot.js
@@ -2,31 +2,60 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 // functions
+const ScraperHandler = require("./chuckScraper")
 const QuoteHandler = require('./source/getQuote')
 const translatorHandler = require('./translator')
 
 // Initialize the bot
+// const { BOT_TOKEN } = process.env;
 const token = '6663098087:AAEsGkUdlZI121bvDh49nhh0q4w-Eq6PXGw';
 const bot = new TelegramBot(token, {polling: true});
+bot.setWebHook()
 
-let currLanguage = "en";
+// Response messages
+const welcomeText = "Welcome to ChuckBot! Please send 'set language [language]' to set your preferred language.";
+const exampleSetLanguageEnglish = "Please provide a valid language preference. For example, 'set language english'.";
+const noProblemTextHebrew = "אין בעיה";
+const oldLanguage = "en";
+const anIntegerBetween1And101 = 'Please send an integer between 1 and 101';
 
-// Start handler
-bot.onText(/\/start/, (msg) => {
-    const chatId = msg.chat.id;
-    bot.sendMessage(chatId, "Welcome to ChuckBot! " +
-        "Send 'set language [language]' to set your preferred language.");
+// Regexps
+const startRegexp = /\/start/;
+const setLanguageRegexp = /set language (.+)/;
+const numberPattern = /^(([0-9])+)$/;
+
+// Global param
+let currLanguage = oldLanguage;
+let initialized = null;
+
+
+// Initialize chuck quotes
+bot.on('message', (msg) => {
+    if (initialized == null) {
+        try {
+            ScraperHandler.chuckScraper();
+            initialized = 1;
+        } catch (e) {
+            console.error(e);
+        }
+    }
 });
 
-// Set language handler
-bot.onText(/set language (.+)/, async (msg, match) => {
+// Start handler
+bot.onText(startRegexp, (msg) => {
     const chatId = msg.chat.id;
-    const userLanguage = match[1].toLowerCase(); // Extracted language preference
+    bot.sendMessage(chatId, welcomeText);
+});
 
-    const exampleSetLanguageEnglish = "Please provide a valid language preference. For example, 'set language english'.";
+
+// Set language handler
+bot.onText(setLanguageRegexp, async (msg, match) => {
+    const chatId = msg.chat.id;
+
+    const userLanguage = match[1].toLowerCase(); // Extracted language preference
     if (userLanguage) {
         // answer first response
-        let answer = await translatorHandler.firstResponse("אין בעיה", userLanguage);
+        let answer = await translatorHandler.firstResponse(noProblemTextHebrew, userLanguage);
         if (answer != null) {
             currLanguage = answer.newLanguageCode;
             bot.sendMessage(chatId, answer.translated);
@@ -41,26 +70,32 @@ bot.onText(/set language (.+)/, async (msg, match) => {
 
 
 // Handle other messages
-
-const numberPattern = /^(([0-9])+)$/;
-
 bot.onText(numberPattern, async (msg, match) => {
         const chatId = msg.chat.id;
         const number = parseInt(match[0]);
-        console.log(number);
 
         if (number >= 1 && number <= 101) {
-            //todo add chuckScraper
             try {
                 let inputText = QuoteHandler.getQuote(number).retValue;
-                let answer = await translatorHandler.returnTranslatedTText(inputText, currLanguage, "en");
+                let answer = await translatorHandler.returnTranslatedTText(inputText, currLanguage, oldLanguage);
                 bot.sendMessage(chatId, answer);
             } catch (e) {
                 bot.sendMessage(chatId, e);
             }
         } else {
             // Not valid int in the specified range.
-            bot.sendMessage(chatId, 'Please send an integer between 1 and 101');
+            bot.sendMessage(chatId, anIntegerBetween1And101);
         }
     }
 );
+
+// Other invalid message
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    const text = msg.text;
+
+    if (!startRegexp.test(text) && !setLanguageRegexp.test(text) && !numberPattern.test(text)) {
+        bot.sendMessage(chatId, "Please choose from one of the options below:\n" +
+            "1." + exampleSetLanguageEnglish + "\n" + "2." + anIntegerBetween1And101);
+    }
+})
